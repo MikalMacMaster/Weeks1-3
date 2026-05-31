f1b862809a18f7521c19bc786089e964c527fe42
LENOVOLEGION\mikal <macmastm@sheridancollege.ca>
2026-05-31 17:57:53 -0400

    Polish

--- Assets/Scripts/Mechanical Drawing/Jellyfish.cs
+++ Assets/Scripts/Mechanical Drawing/Jellyfish.cs
@@ -2,15 +2,31 @@ using UnityEngine;
 
 public class Jellyfish : MonoBehaviour
 {
+    public float floatSpeed = 1f;
+    public float floatAmount = 0.5f;
+
+    public AnimationCurve pulseCurve;
+    public float pulseSpeed = 1f;
+    public float pulseAmount = 0.3f;
+
+    private Vector3 startPosition;
+    private Vector3 startScale;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        startPosition = transform.position;
+        startScale = transform.localScale;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float floatY = Mathf.Sin(Time.time * floatSpeed) * floatAmount;
+
+        transform.position = new Vector3(startPosition.x, startPosition.y + floatY, startPosition.z);
+
+        float curveValue = pulseCurve.Evaluate(Time.time * pulseSpeed);
+
+        transform.localScale = startScale + new Vector3(curveValue * pulseAmount, curveValue * pulseAmount, 0);
     }
 }
